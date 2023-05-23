31c600aff8f93933158b89fe4bf74838c0cc5b7c
Abhilash Joseph <abhilash.pannikottil@gmail.com>
2023-05-23 17:40:21 +0530

    Uploaded Netflix Clone

---
+++ main.js
@@ -0,0 +1,77 @@
+// First FAQ
+
+function faqOpen(){
+    document.getElementById("faq-first-answer").style.display="block";
+    document.getElementById("plus-symbol").style.display="none";
+    document.getElementById("close-symbol").style.display="block";
+}
+function faqClose(){
+    document.getElementById("close-symbol").style.display="none";
+    document.getElementById("faq-first-answer").style.display="none";
+    document.getElementById("plus-symbol").style.display="block";
+}
+
+//Second FAQ
+
+function faqOpen2(){
+    document.getElementById("faq-second-answer").style.display="block";
+    document.getElementById("plus-symbol2").style.display="none";
+    document.getElementById("close-symbol2").style.display="block";
+}
+function faqClose2(){
+    document.getElementById("close-symbol2").style.display="none";
+    document.getElementById("faq-second-answer").style.display="none";
+    document.getElementById("plus-symbol2").style.display="block";
+}
+
+//Third FAQ
+
+function faqOpen3(){
+    document.getElementById("faq-third-answer").style.display="block";
+    document.getElementById("plus-symbol3").style.display="none";
+    document.getElementById("close-symbol3").style.display="block";
+}
+function faqClose3(){
+    document.getElementById("close-symbol3").style.display="none";
+    document.getElementById("faq-third-answer").style.display="none";
+    document.getElementById("plus-symbol3").style.display="block";
+}
+
+//Fourth FAQ
+
+function faqOpen4(){
+    document.getElementById("faq-fourth-answer").style.display="block";
+    document.getElementById("plus-symbol4").style.display="none";
+    document.getElementById("close-symbol4").style.display="block";
+}
+function faqClose4(){
+    document.getElementById("close-symbol4").style.display="none";
+    document.getElementById("faq-fourth-answer").style.display="none";
+    document.getElementById("plus-symbol4").style.display="block";
+}
+
+//Fifth FAQ
+
+function faqOpen5(){
+    document.getElementById("faq-fifth-answer").style.display="block";
+    document.getElementById("plus-symbol5").style.display="none";
+    document.getElementById("close-symbol5").style.display="block";
+}
+function faqClose5(){
+    document.getElementById("close-symbol5").style.display="none";
+    document.getElementById("faq-fifth-answer").style.display="none";
+    document.getElementById("plus-symbol5").style.display="block";
+}
+
+//Sixth FAQ
+
+function faqOpen6(){
+    document.getElementById("faq-sixth-answer").style.display="block";
+    document.getElementById("plus-symbol6").style.display="none";
+    document.getElementById("close-symbol6").style.display="block";
+}
+function faqClose6(){
+    document.getElementById("close-symbol6").style.display="none";
+    document.getElementById("faq-sixth-answer").style.display="none";
+    document.getElementById("plus-symbol6").style.display="block";
+}
